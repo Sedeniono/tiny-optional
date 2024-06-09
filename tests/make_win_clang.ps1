88fdf45f9f527d349f048cb119611d61a3dd585c
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2024-06-09 09:38:27 +0200

    Added test runs with disabled UB tricks to workflows.

--- tests/make_win_clang.ps1
+++ tests/make_win_clang.ps1
@@ -2,7 +2,7 @@
 
 $CXX = "clang++"
 $ADDITIONAL_FLAGS = "-m64", "-std=c++20"
-$OUT_DIR_NAME = "win_clang_x64_cpp17_debug"
+$OUT_DIR_NAME = "win_clang_x64_cpp20_debug"
 
 $script = $PSScriptRoot + "\make_win_gcclike.ps1"
 & $script
