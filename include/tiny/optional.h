b546e3ef0c56b4748a6c23405c7939fb89d76879
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2025-09-23 23:12:46 +0200

    Increased version to 1.5.1.
    When I increased the version to 1.5.0 in c42bc18be85a49bdc382e2b365e284b1dcda3cff, I unfortunately forgot to increase the version in the CMakeLists.txt.
    This commit fixes it, and increases the patch version to 1 to distinguish it from the faulty 1.5.0.

--- include/tiny/optional.h
+++ include/tiny/optional.h
@@ -55,7 +55,7 @@ Original repository: https://github.com/Sedeniono/tiny-optional
 // E.g. TINY_OPTIONAL_VERSION 100301
 //
 #define TINY_OPTIONAL_VERSION_MAJOR_MINOR 1005 // If you change this, adapt Natvis, too!
-#define TINY_OPTIONAL_VERSION_PATCH 0
+#define TINY_OPTIONAL_VERSION_PATCH 1
 #define TINY_OPTIONAL_VERSION (TINY_OPTIONAL_VERSION_MAJOR_MINOR * 100 + TINY_OPTIONAL_VERSION_PATCH)
 
 
